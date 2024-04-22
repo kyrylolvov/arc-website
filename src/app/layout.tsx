e565ce6c545cd48abccf1e41a85c679f78077495
kyrylolvov <kyrylo@arccompute.io>
2024-04-22 19:51:21 -0400

    CHANGED: - Initiated a blank Next.js project with Typescript and TailwindCSS - Added Roboto sans and mono fonts
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "~/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+
+import { roboto, roboto_mono } from "~/assets/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${roboto.variable} ${roboto_mono.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ src/assets/fonts.ts
@@ -0,0 +1,4 @@
+import { Roboto, Roboto_Mono } from "next/font/google";
+
+export const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"], display: "swap", variable: "--font-roboto" });
+export const roboto_mono = Roboto_Mono({ weight: ["400"], subsets: ["latin"], display: "swap", variable: "--font-roboto-mono" });

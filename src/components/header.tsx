8082b4c60d8c6e5e4723b9489496dc5f02c8cf53
kyrylolvov <kyrylo@arccompute.io>
2024-05-09 19:04:01 -0400

    CHANGED: - Finished header component
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import * as React from "react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { cn } from "~/utils/cn";
 import { isLinkNavItem, isMenuNavItem, navItems } from "~/utils/tabs";
@@ -19,32 +20,47 @@ import {
 } from "./ui/navigation-menu";
 
 const hoverBubblePosition: { [key: string]: string } = {
-  products: "w-[106.11px] translate-x-0",
-  resources: "w-[115.65px] translate-x-[106.11px]",
-  docs: "w-[65.01px] translate-x-[229.76px]",
-  contact: "w-[83.28px] translate-x-[298.77px]",
+  products: "w-[98.09px] translate-x-0",
+  resources: "w-[107.63px] translate-x-[100.11px]",
+  docs: "w-[57.01px] translate-x-[213.76px]",
+  contact: "w-[75.28px] translate-x-[274.77px]",
 };
 
 export default function Header() {
-  const [hoveredNavItem, setHoveredNavItem] = useState<string | null>("docs");
+  const pathname = usePathname();
+
+  const activeNavItem = useMemo(() => navItems.find((navItem) => pathname.includes(navItem.slug))?.id, [pathname]);
+
+  const [hoveredNavItem, setHoveredNavItem] = useState<string | null>(null);
+
   const prevNavItemRef = useRef<string | null>(null);
 
   useEffect(() => {
     prevNavItemRef.current = hoveredNavItem;
   }, [hoveredNavItem]);
 
-  console.log(prevNavItemRef.current, hoveredNavItem);
+  useEffect(() => {
+    setHoveredNavItem(activeNavItem ?? null);
+  }, [activeNavItem]);
 
   return (
     <div className="fixed z-30 flex h-[68px] w-full max-w-screen-2xl items-center justify-between border-b bg-background px-6">
       <Link href={"/"}>
         <Icons.Logo className="h-6" />
       </Link>
-      <NavigationMenu onMouseLeave={() => setHoveredNavItem(null)}>
+      <NavigationMenu
+        onMouseLeave={() => {
+          if (activeNavItem) {
+            setHoveredNavItem(activeNavItem);
+          } else {
+            setHoveredNavItem(null);
+          }
+        }}
+      >
         <div
           className={cn(
-            "absolute left-0 top-0 h-10 rounded-full bg-accent",
-            hoveredNavItem ? `${hoverBubblePosition[hoveredNavItem]} duration-[300ms] transition-all` : "opacity-0",
+            "absolute left-0 top-0 h-[32px] rounded-full bg-accent",
+            hoveredNavItem ? `${hoverBubblePosition[hoveredNavItem]} transition-all duration-300` : "opacity-0",
             prevNavItemRef.current === null && "duration-0",
           )}
         ></div>
@@ -57,7 +73,12 @@ export default function Header() {
 
               return (
                 <NavigationMenuItem key={id} onMouseOver={() => setHoveredNavItem(id)}>
-                  <NavigationMenuTrigger onClick={(e) => e.preventDefault()}>{label}</NavigationMenuTrigger>
+                  <NavigationMenuTrigger
+                    onClick={(e) => e.preventDefault()}
+                    className={cn(activeNavItem === id && "rounded-full bg-accent text-accent-foreground")}
+                  >
+                    {label}
+                  </NavigationMenuTrigger>
                   <NavigationMenuContent>
                     <div className="grid">
                       <div className={cn("grid w-[40rem] p-2", lowerLinks && "border-b")}>
@@ -126,7 +147,14 @@ export default function Header() {
             return (
               <NavigationMenuItem key={id} onMouseOver={() => setHoveredNavItem(id)}>
                 <Link href={isLinkNavItem(navItem) ? navItem.url : `/${slug}`} legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>{label}</NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      activeNavItem === id && "bg-accent text-accent-foreground",
+                    )}
+                  >
+                    {label}
+                  </NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
             );

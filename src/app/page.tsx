a04f6710f2d11744404d8b71de544d9d8b5cb6ba
kyrylolvov <kyrylo@arccompute.io>
2024-05-11 10:45:58 -0400

    CHANGED: - Added new section to home page
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex max-w-5xl flex-col items-center px-8 py-24">
+    <main className="mx-auto flex max-w-6xl flex-col items-center px-8 py-24">
       <div className="flex items-center gap-4">
         <p className="font-light text-muted-foreground">
           <span className="font-medium text-primary">NVIDIA H200</span> GPU servers are now available for{" "}
@@ -37,9 +37,11 @@ export default function Home() {
       </div>
 
       <div className="mt-16 grid grid-cols-3 gap-6">
-        <div className="group grid cursor-pointer gap-2 rounded-lg border bg-white p-4 transition-colors hover:border-primary dark:bg-black">
-          <p className="text-sm text-muted-foreground">Event</p>
-          <p className="font-semibold">Join us in Hamburg, Germany, for ISC24!</p>
+        <div className="group flex cursor-pointer flex-col justify-between rounded-lg border bg-white p-4 transition-colors hover:border-primary dark:bg-black">
+          <div className="grid gap-2">
+            <p className="text-sm text-muted-foreground">Event</p>
+            <p className="font-semibold">Join us next week in Hamburg, Germany, for ISC24!</p>
+          </div>
           <div className="relative mt-2 flex h-8 w-12 items-center overflow-hidden rounded-full bg-secondary text-xs text-primary">
             <RiArrowRightLine className="absolute left-[50%] h-4 w-4 shrink-0 translate-x-[-50%] transition-transform duration-300 ease-out group-hover:translate-x-[150%]" />
             <RiArrowRightLine className="absolute left-[50%] h-4 w-4 shrink-0 translate-x-[-250%] transition-transform duration-300 ease-out group-hover:translate-x-[-50%]" />
@@ -113,6 +115,67 @@ export default function Home() {
                 Lower Cost<span className="text-muted-foreground">*</span>
               </span>
             </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-32 w-full">
+        <h2 className="text-center text-3xl font-semibold">Unlock the True Potential of Your GPU Infrastructure</h2>
+        <p className="mx-auto mt-2 max-w-[90%] text-center font-light text-muted-foreground">
+          An advanced GPU management solution that maximizes user and task density, optimizing GPU utilization and
+          performance across your data center.
+        </p>
+        <div className="mt-8 grid grid-cols-2 gap-12">
+          <div className="flex aspect-square w-full flex-col justify-between rounded-lg border bg-white p-8 dark:bg-black">
+            <div className="grid gap-8">
+              <div className="-ml-0.5 w-fit rounded-full border px-4 py-2 font-mono text-sm">Manage</div>
+              <div className="grid gap-6">
+                <div className="grid gap-2">
+                  <h2 className="text-4xl font-semibold">Nexus</h2>
+                  <p className="text-lg font-medium">
+                    Creates and manages your environment while optimizing GPU utilization and performance, enables
+                    administrators to enhance user and task density.
+                  </p>
+                </div>
+                <ul className="grid gap-1 text-muted-foreground">
+                  <li>- Intercepts machine code at the load stage dedicated to the accelerated hardware</li>
+                  <li>
+                    - Aware of the instructions for execution and the data size, but not the data contents, maintaining
+                    data privacy.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <Button size="sm" className="group flex w-fit gap-1">
+              Learn more
+              <RiArrowRightLine className="mt-[1px] h-4 w-4 transition-transform duration-150 ease-out group-hover:-rotate-45" />
+            </Button>
+          </div>
+
+          <div className="flex aspect-square w-full flex-col justify-between rounded-lg border bg-white p-8 dark:bg-black">
+            <div className="grid gap-8">
+              <div className="-ml-0.5 w-fit rounded-full border px-4 py-2 font-mono text-sm">Automate</div>
+              <div className="grid gap-6">
+                <div className="grid gap-2">
+                  <h2 className="text-4xl font-semibold">Oracle</h2>
+                  <p className="text-lg font-medium">
+                    Automates task matching and task deployment across your cluster, by managing low-level operational
+                    execution of instructions
+                  </p>
+                </div>
+                <ul className="grid gap-1 text-muted-foreground">
+                  <li>- The source code is saved to the host memory, independent of Nexus{"'"}s operation.</li>
+                  <li>
+                    - Pairs, matches tasks, and staggers kernel execution times using the information intercepted from
+                    machine codes.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <Button size="sm" className="group flex w-fit gap-1">
+              Learn more
+              <RiArrowRightLine className="mt-[1px] h-4 w-4 transition-transform duration-150 ease-out group-hover:-rotate-45" />
+            </Button>
           </div>
         </div>
       </div>
